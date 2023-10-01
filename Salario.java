7a9092cda36372d67ef1663bddb4eed791e505e8
Pedro Cunegundes <bangue062@gmail.com>
2023-09-30 21:34:45 -0300

    novo commit

---
+++ Salario.java
@@ -0,0 +1,12 @@
+public class Salario{
+    // Estamos definindo se o usuario ganha bem ou não
+      public static void main(String[] args) {
+        int salario = 1500;
+        if (salario > 3000) {
+          System.out.println("Voçe esta ganhando bem");
+        }  else{
+            System.out.println("Voçe ainda não tem um salario legal");
+        }
+      }
+    }
+    
