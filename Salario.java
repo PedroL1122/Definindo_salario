4da6ffe9b413ca3bf967cb142c5c42384843d878
Pedro Cunegundes <95534653+PedroL1122@users.noreply.github.com>
2023-10-01 22:22:50 -0300

    Update Salario.java

--- Salario.java
+++ Salario.java
@@ -1,12 +1,47 @@
-public class Salario{
-    // Estamos definindo se o usuario ganha bem ou não
-      public static void main(String[] args) {
-        int salario = 1500;
-        if (salario > 3000) {
-          System.out.println("Voçe esta ganhando bem");
-        }  else{
-            System.out.println("Voçe ainda não tem um salario legal");
+ public class Informaçao_pessoa {
+
+        // Propriedades
+
+      String cargo;
+
+      int idade;
+
+      String nome;
+
+      int Salario;
+
+
+
+        // Construtor
+
+        public Informaçao_pessoa(String cargo, int idade, String nome) {
+
+            this.cargo = "Gerente";
+
+            this.idade = 19;
+
+            this.nome = "Pedro";
+
+            this.Salario = 1500;
+
         }
-      }
-    }
-    
+
+
+
+        // aqui esta o metodo para exibir informaçoes da pessoa
+
+        public void exibirInformacoes() {
+
+            System.out.println("Cargo: " + cargo);
+
+            System.out.println("Idade: " + idade);
+
+            System.out.println("nome: " + nome);
+
+            if (Salario > 3000){
+              System.out.println("Voçe esta ganhando bem"); }
+              else{
+                System.out.println("Ainda não esta ganhando tão bem");
+            }
+        }
+ }
